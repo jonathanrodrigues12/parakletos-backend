057f0fa930368022f34e2e35a97c5947460ea979
Jonathan Rodrigues <jonathanrodrigues12@gmail.com>
2024-06-04 20:05:33 -0300

    configuration Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(8080);
+
+  const config = new DocumentBuilder()
+    .setTitle('Parakletos API')
+    .setDescription('API of intregration')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, document);
 }
 bootstrap();
